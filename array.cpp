672882cf2c5b2e83051508d9c370d0eb23484bec
neelkanthsahu <neelkanthsahu1503@gmail.com>
2024-10-09 15:53:43 +0530

    array cpp file

--- array.cpp
+++ array.cpp
@@ -4,11 +4,13 @@ using namespace std;
 int main(){
     int arr[10];
     for(int i=0; i<10; i++){
+        cout<<"Enter values";
         cin>>arr[i];
     }
      for(int i=0; i<10; i++){
         cout<<arr[i];
     }
+    cout<<"this is new line";
 
 
     return 0;
